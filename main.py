e55c6b6c6c1e455ce3f0669bf169875a47cef107
joonas <joonasmpitkaniemi@gmail.com>
2020-02-21 20:39:39 +0200

    Wrote

---
+++ main.py
@@ -0,0 +1,17 @@
+from pandas import read_csv
+from matplotlib import pyplot
+
+def load_dataset():
+    url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+    names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+    dataset = read_csv(url, names=names)
+
+    dataset.plot(kind='bar', subplots=True, layout=(2,2), sharex=False, sharey=False)
+    pyplot.show()
+
+def main():
+    load_dataset()
+
+
+if __name__ == "__main__":
+    main()
